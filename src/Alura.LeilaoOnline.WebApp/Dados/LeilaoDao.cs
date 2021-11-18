85d2af45babd7ecb12ea07fe685e3c2745a95201
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-11-18 19:27:58 -0300

    Criado classe Dao

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,37 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao // classe resposavel de acesso aos objetos(responsável por manter as operações) 
+   
+    {
+        AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+
+        public IEnumerable<Categoria> BuscarCategorias()// Vai trazer uma lista de categorias, recupera lista
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria)
+                .ToList();
+        }
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.First(l => l.Id == id);
+        }
+    }
+}
